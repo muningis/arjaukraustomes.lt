338f72f8053d1321338ff815cc53335d58c2330e
Rokas Muningis <28229273+RokasMuningis@users.noreply.github.com>
2019-01-03 18:39:48 +0200

    Index page, _document wrapper, assets

---
+++ pages/_document.tsx
@@ -0,0 +1,34 @@
+import Document, { Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <html>
+                <Head>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+                    <title>Ar jau kraustomės?</title>
+                    <style>{`
+                        html, body { margin: 0 }
+                        html,
+                        body,
+                        div#__next {
+                            height: 100%;
+                        }
+                    `}</style>
+                    <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
+                    <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
+                    <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
+                    <link rel="manifest" href="/static/site.webmanifest" />
+                    <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#5bbad5" />
+                    <meta name="msapplication-TileColor" content="#da532c" />
+                    <meta name="theme-color" content="#ff6b00" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </html>
+        );
+    }
+}
---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Ne = styled.span`
+    font-size: 128px;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: Helvetica;
+    background: #ff6b00;
+    color: white;
+    text-transform: uppercase;
+    height: 100%;
+`;
+
+export default () => <Ne>ne</Ne>
