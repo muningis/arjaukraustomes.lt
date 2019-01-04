74d32825f375f475a1a64d37a0fe7d204f50b2b6
Rokas Muningis <28229273+RokasMuningis@users.noreply.github.com>
2019-01-04 09:28:06 +0200

    Add meta description and lang. Update manifesto

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Head, Main, NextScript } from 'next/document';
 export default class MyDocument extends Document {
     render() {
         return (
-            <html>
+            <html lang="lt">
                 <Head>
                     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                     <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
@@ -14,10 +14,11 @@ export default class MyDocument extends Document {
                     <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
                     <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
                     <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
-                    <link rel="manifest" href="/static/site.webmanifest" />
+                    <link rel="manifest" href="/static/site.manifest" />
                     <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#5bbad5" />
                     <meta name="msapplication-TileColor" content="#da532c" />
                     <meta name="theme-color" content="#ff6b00" />
+                    <meta name="description" content="Ar mes jau kraustomės į naują offisą?"></meta>
                 </Head>
                 <body>
                     <Main />
