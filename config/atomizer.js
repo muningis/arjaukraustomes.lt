7a0a458cb43d793453df50ee8290098af4d7360a
Rokas Muningis <28229273+RokasMuningis@users.noreply.github.com>
2019-01-17 18:31:56 +0200

    Add font

--- config/atomizer.js
+++ config/atomizer.js
@@ -2,6 +2,8 @@ module.exports = {
     custom: {
         'gray-dark': '#4f4c4c',
         'green-normal': '#2ecc71',
-        'blue-light': '#f6fbfe'
+        'blue-light': '#f6fbfe',
+
+        font: 'PFDinTextPro, Helvetica, sans-serif'
     }
 }
