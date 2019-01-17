148395c9b4c6c427695d82e1a43741eef149fb4d
Rokas Muningis <28229273+RokasMuningis@users.noreply.github.com>
2019-01-17 22:40:24 +0200

    change path of sw.js

--- src/service_worker.ts
+++ src/service_worker.ts
@@ -1,7 +1,7 @@
 export default class ServiceWorkerFactory {
     constructor() {
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('/sw.js', { scope: "./static/js" });
+            navigator.serviceWorker.register('/static/js/sw.js', { scope: "./static/js" });
         }
     }
 }
