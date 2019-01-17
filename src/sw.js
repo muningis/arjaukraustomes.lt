23315f6f51a48c34861d6e9531ce2658ac40b6d3
Rokas Muningis <28229273+RokasMuningis@users.noreply.github.com>
2019-01-17 12:49:37 +0200

    clear cache

--- src/sw.js
+++ src/sw.js
@@ -47,9 +47,7 @@ self.addEventListener('activate', function (event) {
     caches.keys().then(function (cacheNames) {
       return Promise.all(
         cacheNames.filter(function (cacheName) {
-          // Return true if you want to remove this cache,
-          // but remember that caches are shared across
-          // the whole origin
+          return cacheName === 'pwabuilder-offline';
         }).map(function (cacheName) {
           return caches.delete(cacheName);
         })
