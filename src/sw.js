7aadf6c2d79a6610e4fb9b658828cbcf029284c0
Rokas Muningis <28229273+RokasMuningis@users.noreply.github.com>
2019-01-18 16:11:26 +0200

    use pwabuilder precache sw

--- src/sw.js
+++ src/sw.js
@@ -1,48 +1,42 @@
-const CACHE_NANE = 'ar-jau-kraustomes_v4';
-const NO_MATCH_ERROR_CODE = 404;
-const NO_MATCH_REJECT_REASON = 'catched file was not found';
+var CACHE = 'ar-jau-kraustomes_v5';
+var PRECACHE_FILES = [
+    'index.html',
+    'static/css/style.css'
+];
 
 self.addEventListener('install', function (event) {
-    const index_page = new Request('index.html');
-    event.waitUntil(
-        fetch(index_page).then(async function (response) {
-            const cache = await caches.open(CACHE_NANE);
-            return cache.put(index_page, response);
-        }));
+    event.waitUntil(precache().then(function () {
+        return self.skipWaiting();
+    }));
+});
+
+self.addEventListener('activate', function () {
+    return self.clients.claim();
 });
 
 self.addEventListener('fetch', function (event) {
-    const update_cache = async function (request) {
-        const cache = await caches.open(CACHE_NANE);
-        const req = request.clone();
-        const response = await fetch(req);
-        return cache.put(request, response);
-    };
-
-    event.waitUntil(update_cache(event.request));
-
-    event.respondWith(
-        fetch(event.request).catch(async function () {
-            const cache = await caches.open(CACHE_NANE);
-            const matching = await cache.match(event.request);
-            const report = !matching || matching.status == NO_MATCH_ERROR_CODE
-                ? Promise.reject(NO_MATCH_REJECT_REASON)
-                : matching;
-            return report;
-        })
-    );
+    event.respondWith(from_cache(event.request).catch(from_server(event.request)));
+    event.waitUntil(update(event.request));
 });
 
-self.addEventListener('activate', function(event) {
-    event.waitUntil(
-      caches.keys().then(function(cache_names) {
-        return Promise.all(
-            cache_names.filter(function(cache_name) {
-            return cache_name !== CACHE_NANE
-          }).map(function(cache_name) {
-            return caches.delete(cache_name);
-          })
-        );
-      })
-    );
-  });
+async function precache() {
+    const cache = await caches.open(CACHE);
+    return cache.addAll(PRECACHE_FILES);
+}
+
+async function from_cache(request) {
+    const cache = await caches.open(CACHE);
+    const matching = await cache.match(request);
+    return matching || Promise.reject('no-match');
+}
+
+async function update(request) {
+    const cache = await caches.open(CACHE);
+    const response = await fetch(request);
+    return cache.put(request, response);
+}
+
+async function from_server(request) {
+    const response = await fetch(request);
+    return response;
+}
