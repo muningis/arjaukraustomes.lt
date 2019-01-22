9417fc058b57c7b6e0d2b7808378d77532f5df89
Rokas Muningis <28229273+RokasMuningis@users.noreply.github.com>
2019-01-22 21:21:17 +0200

    fix classnames

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,6 @@ import CountDown from './count_down';
 import ServiceWorkerFactory from './service_worker';
 import Tracker from './tracker';
 
-Tracker.trackPageLoad();
+// Tracker.trackPageLoad();
 new CountDown(1548050400000, '.js-15a');
 new ServiceWorkerFactory();
