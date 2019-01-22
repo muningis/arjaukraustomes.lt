6ba33d713db57531b746d5c1f169a12fc0560bd1
Rokas Muningis <28229273+RokasMuningis@users.noreply.github.com>
2019-01-22 21:28:54 +0200

    update info and timers

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import CountDown from './count_down';
 import ServiceWorkerFactory from './service_worker';
 
-new CountDown(1547823600000, '.js-15a');
+new CountDown(1548342000000, '.js-1133a');
+new CountDown(1548428400000, '.js-313a');
 new ServiceWorkerFactory();
